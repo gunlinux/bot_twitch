bdfd6878b7bc834a1b13e5c5b9610d46a561843a
loki <gunlinux@ya.ru>
2026-04-15 23:44:34 +0300

    recommands

--- retwitch_worker.py
+++ retwitch_worker.py
@@ -6,7 +6,6 @@ from retwitch.handlers import RetwitchEventHandler
 from requeue.fstream.consumer import RabbitConsumer
 from requeue.sender.sender import Sender
 from retwitch.handlers import Command
-from commander import get_commands_from_dir, reload_command
 from commander.commands import auf
 from retwitch import settings
 from retwitch.utils import logger_setup
@@ -22,19 +21,21 @@ async def main() -> None:
     retwitch_handler: RetwitchEventHandler = RetwitchEventHandler(
         sender=sender,
         admin='gunlinux',
+        command_dir=settings.COMMAND_DIR,
     )
-    Command('ауф', retwitch_handler, real_runner=auf)
-    Command('gunlinAuf', retwitch_handler, real_runner=auf)
-    Command('awoo', retwitch_handler, real_runner=auf)
-    Command('auf', retwitch_handler, real_runner=auf)
-    get_commands_from_dir(settings.COMMAND_DIR, retwitch_handler)
-    reload_command_runner = reload_command(settings.COMMAND_DIR, retwitch_handler)
-    Command('$reload', retwitch_handler, real_runner=reload_command_runner)
+    commands = [
+        Command('ауф', real_runner=auf),
+        Command('gunlinAuf', real_runner=auf),
+        Command('awoo', real_runner=auf),
+        Command('auf', real_runner=auf),
+    ]
+    for command in commands:
+        retwitch_handler.register(command)
 
     await RabbitConsumer(
         broker=broker,
         worker=retwitch_handler.on_message,
-        queue_name=settings.DONATS_EVENTS,
+        queue_name=settings.TWITCH_EVENTS,
     ).consume()
 
 
