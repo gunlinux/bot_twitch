be29ea2c40073048813e141e7b803169cb8e8309
loki <gunlinux@ya.ru>
2026-05-26 20:25:38 +0300

    remove magic

--- retwitch/models/__init__.py
+++ retwitch/models/__init__.py
@@ -1,28 +1,9 @@
-import typing
 import time
-from dataclasses import dataclass, fields as dfields, field
+from dataclasses import dataclass, field
 
 
-class Magic:
-    def get_attr(self, name: str) -> str:
-        if name.endswith('token') or 'secret' in name.lower():
-            v = getattr(self, name)
-            return f'secret: #### {v[-4:]}'
-        return getattr(self, name)
-
-    @typing.override
-    def __str__(self):
-        # Manually build the default dataclass output
-        field_values = [f'{f.name}={self.get_attr(f.name)!r}' for f in dfields(self)]  # pyright: ignore[reportArgumentType]
-        return f'{self.__class__.__name__}({", ".join(field_values)})'
-
-    @typing.override
-    def __repr__(self):
-        return self.__str__()
-
-
-@dataclass
-class TokenResponse(Magic):
+@dataclass(repr=False)
+class TokenResponse:
     access_token: str
     expires_in: int
     token_type: str
